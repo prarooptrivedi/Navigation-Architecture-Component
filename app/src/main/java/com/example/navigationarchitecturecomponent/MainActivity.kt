ee1323645d07cf0b9d942eee993d9a1e8230739b
praroop <praroop@galtonanalytics.com>
2020-05-19 22:28:12 +0530

    First Commit

--- app/src/main/java/com/example/navigationarchitecturecomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationarchitecturecomponent/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.navigationarchitecturecomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navigationarchitecturecomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }
