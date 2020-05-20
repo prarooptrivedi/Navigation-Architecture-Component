d38e5d9418f28480ffcbdfd0a7376d3fcfd8bbcf
praroop <praroop@galtonanalytics.com>
2020-05-20 17:11:44 +0530

    First Commit

--- app/src/main/java/com/example/navigationarchitecturecomponent/HomeFragment.kt
+++ app/src/main/java/com/example/navigationarchitecturecomponent/HomeFragment.kt
@@ -2,12 +2,17 @@ package com.example.navigationarchitecturecomponent
 
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.navigationarchitecturecomponent.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +26,18 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_home, container, false)
         databinding=DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        databinding.button.setOnClickListener{
+            if (!TextUtils.isEmpty(databinding.editText.text.toString())){
+
+
+            val bundle= bundleOf("user_input" to databinding.editText.text.toString())
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment2,bundle)
+            }
+            else{
+                Toast.makeText(activity,"Please Insert Your Name",Toast.LENGTH_LONG).show()
+            }
+
+        }
         return databinding.root
     }
 
--- app/src/main/java/com/example/navigationarchitecturecomponent/SecondFragment.kt
+++ app/src/main/java/com/example/navigationarchitecturecomponent/SecondFragment.kt
@@ -6,18 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigationarchitecturecomponent.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class SecondFragment : Fragment() {
-
+        private lateinit var dataBinding:FragmentSecondBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        dataBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+       var input=requireArguments().getString("user_input")
+        dataBinding.textName.text=input.toString()
+        return  dataBinding.root
     }
 
 
